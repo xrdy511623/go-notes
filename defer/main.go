12363f504e0d506b72f2cd1b48ef682170be3a3c
qiujun <jun.qiu@funpluspartners.com>
2023-11-10 18:29:54 +0800

    add more cases for defer

--- defer/main.go
+++ defer/main.go
@@ -20,25 +20,12 @@ func df1() int {
 func df2() int {
 	x := 5
 	defer func() {
-		x += 1
-		//x = x + 1 // 改的是内部变量x，不是返回值
-	}()
-	return x
-}
-
-func df3() int {
-	x := 5
-	defer func() {
 		x = 6
 	}()
 	return x
 }
 
-/*
-
- */
-
-func df4() (x int) {
+func df3() (x int) {
 	defer func() {
 		x++ // 修改的就是返回值x
 	}()
@@ -48,7 +35,7 @@ func df4() (x int) {
 /*
 第一步，返回值赋值x=5,第二步defer语句执行，此时修改的就是返回值x，x=6；最后return返回的就是6
 */
-func df5() (y int) {
+func df4() (y int) {
 	x := 5
 	defer func() {
 		x++
@@ -61,7 +48,7 @@ func df5() (y int) {
 作为x的副本y并不会随之发生变化
 所以最后return返回的就是y=5
 */
-func df6() (x int) {
+func df5() (x int) {
 	defer func(x int) {
 		x++
 	}(x)
@@ -69,9 +56,51 @@ func df6() (x int) {
 }
 
 /*
-第一步，返回值赋值x=5,第二步defer语句执行，此时内部这个匿名函数修改的是自己内部的函数变量x，与外部函数df4的变量x无关，所以最后返回的还是5
+第一步，返回值赋值x=5,第二步defer语句执行，此时内部这个匿名函数修改的是自己内部的函数变量x，与外部函数df6的变量x无关，所以最后返回的还是5
 所以最后return返回的就是x=5
 */
+
+func df6() {
+	var a = 1
+	defer fmt.Println(a)
+	a = 2
+	return
+}
+
+func df7() {
+	i := 0
+	defer func() {
+		fmt.Println(i)
+	}()
+	i++
+}
+
+func df8() {
+	i := 0
+	defer func(i int) {
+		fmt.Println(i)
+	}(i)
+	i++
+}
+
+func df9() {
+	arr := [3]int{1, 2, 3}
+	defer func(array *[3]int) {
+		for i := range array {
+			fmt.Println(array[i])
+		}
+	}(&arr)
+	arr[0] = 10
+}
+
+func df10() {
+	defer func() {
+		defer func() {
+			fmt.Println("B")
+		}()
+		fmt.Println("A")
+	}()
+}
 
 func calc(index string, a, b int) int {
 	ret := a + b
@@ -85,7 +114,11 @@ func main() {
 	fmt.Println(df3())
 	fmt.Println(df4())
 	fmt.Println(df5())
-	fmt.Println(df6())
+	df6()
+	df7()
+	df8()
+	df9()
+	df10()
 	/*
 		Go语言中的defer语句会将其后面跟随的语句进行延迟处理。在defer归属的函数即将返回或退出时，将延迟处理的语句按
 		defer定义的逆序进行执行，也就是说，先被defer的语句最后被执行，最后被defer的语句，最先被执行。
